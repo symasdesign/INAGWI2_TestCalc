5081f5cf7d70f419e29893caa8f434c34d0db8e7
Thomas Kehl <tk@symas.ch>
2021-06-12 15:02:43 +0200

    initial

---
+++ Calc/src/Calc.java
@@ -0,0 +1,7 @@
+
+public class Calc {
+
+	public int add(int val1, int val2) {
+		return val1 * val2;
+	}
+}
---
+++ Calc/src/CalcTest.java
@@ -0,0 +1,22 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
+class CalcTest {
+
+	@Test
+	void test() {
+		// arrange
+		Calc calc = new Calc();
+		
+		// act
+		int res = calc.add(5 , 7);
+		
+		// assert
+		Assert.assertEquals(12, res);
+		
+		// test
+	}
+
+}
